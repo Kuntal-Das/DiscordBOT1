edc2130108e0c65424d8259fd76d5bd1f0f50f6d
Kuntal Das <25ckuntaldas@gmail.com>
2020-10-07 20:44:56 +0530

    comments added

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ const { prefix, token } = require('./config.json');
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
+//commandFiles is a array of file names which contains commands
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+console.log(commandFiles);
 
+//maping commandFiles into collection(client.commands)
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
   client.commands.set(command.name, command);
 }
-
-
 
 client.once('ready', () => console.log('READY'));
 
@@ -20,10 +21,11 @@ client.on('message', message => {
   //ignores messages without prefix and its own messages
   if (!message.content.startsWith(prefix) || message.author.bot) return;
 
+  //splits commands in command and arguments
   const args = message.content.slice(prefix.length).trim().split(/ +/);
   const command = args.shift().toLowerCase();
 
-  console.log(`${command}(${args})`);
+  console.log(`${command}(${args})`);//print requested functions
 
   if (!client.commands.has(command)) return;
 
@@ -33,7 +35,6 @@ client.on('message', message => {
     console.error(error);
     message.reply('there was an error trying to execute that command!');
   }
-
 
 });
 
